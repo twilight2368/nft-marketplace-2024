8ea9d59fc4aa63dfd21a660835becec3c0f0f709
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-06-29 13:07:37 +0700

    now

--- backend/src/controllers/dataController.js
+++ backend/src/controllers/dataController.js
@@ -1,5 +1,6 @@
 const env = require("dotenv");
 const pool = require("../connection/database");
+const bcrypt = require("bcrypt");
 const { json } = require("express");
 env.config();
 
@@ -64,6 +65,214 @@ module.exports.user_get = async (req, res) => {
   }
 };
 
-module.exports.get_artist = async (req, res) => {
+module.exports.artist_get = async (req, res) => {
   //do something
+
+  const artist_id = req.params.id;
+
+  try {
+    const data_1 = await pool.query(
+      "select user_name, account_name, gender, age, email from users where user_id = $1",
+      [artist_id]
+    );
+
+    if (data_1.rowCount === 0) {
+      throw new Error("No user found");
+    }
+
+    const data_2 = await pool.query(
+      "select nft_id, nft_name, image_url, price from nfts where creator = $1",
+      [artist_id]
+    );
+
+    const data_3 = await pool.query(
+      "select nft_id, nft_name, image_url, price from nfts where owner = $1",
+      [artist_id]
+    );
+
+    res.status(200).json({
+      user_data: data_1.rows[0],
+      created_nfts: data_2.rows,
+      owned_nft: data_3.rows,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      message: error.messages,
+    });
+  }
+};
+
+module.exports.user_dashboard_get = async (req, res) => {
+  const userid = req.params.id;
+
+  try {
+    const data_1 = await pool.query(
+      "select nft_id, nft_name, iamge_url, price from nfts where owner = $1",
+      [userid]
+    );
+
+    const data_2 = await pool.query(
+      "select nft_id, nft_name, iamge_url, price from nfts where creator = $1",
+      [userid]
+    );
+
+    res.status(200).json({
+      created_nfts: data_2.rows,
+      owned_nfts: data_1.rows,
+    });
+  } catch (error) {
+    re.status(400).json({
+      message: "Bad request",
+    });
+  }
+};
+
+module.exports.change_username_post = async (req, res) => {
+  console.log(req.body);
+
+  const new_username = req.body.username;
+  const userid = req.body.userid;
+
+  try {
+    const update = await pool.query(
+      "update users set (user_name = $1) where user_id = $2 returning user_name",
+      [new_username, userid]
+    );
+
+    console.log(update);
+
+    res.status(200).json({
+      message: "updated successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.messages,
+    });
+  }
+};
+
+module.exports.change_password_post = async (req, res) => {
+  console.log(req.body);
+  try {
+    const userid = req.body.userid;
+    const salt = await bcrypt.genSalt(10);
+    const hashpass = await bcrypt.hash(req.body.password, salt);
+
+    const update = await pool.query(
+      "update users set (password = $1) where user_id = $2",
+      [hashpass, userid]
+    );
+
+    //console.log(update);
+
+    res.status(200).json({
+      message: "updated successfully",
+    });
+  } catch (error) {
+    res.status(400).json({ message: "bad request" });
+  }
+};
+
+module.exports.change_gender_age_post = async (req, res) => {
+  console.log(req.body);
+
+  const userid = req.body.userid;
+  const user_age = req.body.age;
+  const gender = req.body.gender;
+
+  try {
+    if (user_age <= 13 || !(gender === 0 || gender === 1)) {
+      throw new Error("Bad request");
+    }
+
+    const update = await pool.query(
+      "update users set (age = $1, gender = $2) where user_id = $3",
+      [user_age, gender, userid]
+    );
+
+    res.status(200).json({
+      message: "Update successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.messages,
+    });
+  }
+};
+
+module.exports.change_email_post = async (req, res) => {
+  console.log(req.body);
+
+  const new_email = req.body.email;
+  const userid = req.body.userid;
+
+  try {
+    const update = await pool.query(
+      "update users set (email = $1) where user_id = $2 returning email",
+      [new_email, userid]
+    );
+
+    console.log(update);
+
+    res.status(200).json({
+      message: "updated successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.messages,
+    });
+  }
+};
+
+module.exports.change_nft_sale_post = async (req, res) => {
+  const nft_id = req.body.nft_id;
+
+  try {
+    const update = pool.query("update nfts set (sale = t) where nft_id = $1", [
+      nft_id,
+    ]);
+
+    res.status(200).json({
+      message: "Updated successful",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+};
+
+module.exports.nft_not_sale_user_get = async (req, res) => {
+  console.log(req.params["userid"]);
+  const user_id = req.params["userid"];
+  try {
+    const data = await pool.query(
+      "select nft_id, nft_name, image_url, price from nfts where owner = $1 and sale = 0",
+      [user_id]
+    );
+
+    res.status(200).json(data.rows);
+  } catch (error) {
+    res.status(400).json({
+      message: error.messages,
+    });
+  }
+};
+
+module.exports.sale_nft_by_users_get = async (req, res) => {
+  const userid = req.params["userid"];
+
+  try {
+    const data = await pool.query(
+      "select nft_id, nft_name, image_url, price from nfts where sale = 1 and owner = $1",
+      [userid]
+    );
+
+    res.json(data.rows);
+  } catch (error) {
+    res.status(400).json({
+      message: error.messages,
+    });
+  }
 };
--- backend/src/routes/dataRoute.js
+++ backend/src/routes/dataRoute.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dataRoute = express();
 const dataController = require("../controllers/dataController");
+const requireAuth = require("../middlewares/authRequire");
 
 dataRoute.get("/allnfts", dataController.nft_get);
 
 dataRoute.get("/nft/:id", dataController.one_nft_get);
 
-dataRoute.get("/user/:id", dataController.user_get);
+dataRoute.get("/account/:id", requireAuth, dataController.user_get);
+
+dataRoute.get("/user/:id", dataController.artist_get);
+
+dataRoute.get(
+  "account-data/:id",
+  requireAuth,
+  dataController.user_dashboard_get
+);
 
 module.exports = dataRoute;
